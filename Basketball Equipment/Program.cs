637937b69c8f0a95d1a3be826011590fce69306c
Boris Kisiov <137154006+boriskisiov@users.noreply.github.com>
2023-09-15 10:56:49 +0300

    Add files via upload

---
+++ Basketball Equipment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Transactions;
+
+namespace Basketball_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int AnnualFee = int.Parse(Console.ReadLine());
+
+            double SneakersPrice = AnnualFee * 0.60;
+            double TeamPrice = SneakersPrice * 0.80;
+            double BallPrice = TeamPrice * 0.25;
+            double AccessoriesPrice = BallPrice * 0.20;
+            double TotalPrise = AnnualFee + SneakersPrice+ TeamPrice + BallPrice + AccessoriesPrice;
+
+            Console.WriteLine(TotalPrise);
+        }
+    }
+}
